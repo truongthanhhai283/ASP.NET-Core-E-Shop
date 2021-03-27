f360c24291b6b24ffeb48df8d288ee7c429e42d7
TruongThanhHai28 <truongthanhhai283@gmail.com>
2021-03-27 16:04:03 +0700

    42. Login member user

--- ShopOnline_Solution/ShopOnlineApp/Models/AccountViewModels/LoginViewModel.cs
+++ ShopOnline_Solution/ShopOnlineApp/Models/AccountViewModels/LoginViewModel.cs
@@ -9,7 +9,7 @@ namespace ShopOnlineApp.Models.AccountViewModels
     public class LoginViewModel
     {
         [Required]
-        public string UserName { get; set; }
+        public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
