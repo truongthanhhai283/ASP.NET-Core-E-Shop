fe236dbde3b172c60594c0837a80575d7d19e60a
TruongThanhHai28 <truongthanhhai283@gmail.com>
2021-03-27 12:40:57 +0700

    39. Load product catalog

---
+++ ShopOnline_Solution/ShopOnlineApp/Controllers/Components/PagerViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopOnlineApp.Utilities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopOnlineApp.Controllers.Components
+{
+    public class PagerViewComponent : ViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync(PagedResultBase result)
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", result));
+        }
+    }
+}
--- ShopOnline_Solution/ShopOnlineApp/Controllers/ProductController.cs
+++ ShopOnline_Solution/ShopOnlineApp/Controllers/ProductController.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnlineApp.Models.ProductViewModels;
+using ShopOnlineApp.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace ShopOnlineApp.Controllers
 {
     public class ProductController : Controller
     {
+        IProductService _productService;
+        IProductCategoryService _productCategoryService;
+        IConfiguration _configuration;
+        public ProductController(IProductService productService,IConfiguration configuration,
+            IProductCategoryService productCategoryService)
+        {
+            _productService = productService;
+            _productCategoryService = productCategoryService;
+            _configuration = configuration;
+        }
         [Route("products.html")]
         public IActionResult Index()
         {
@@ -11,9 +28,26 @@ namespace ShopOnlineApp.Controllers
         }
 
         [Route("{alias}-c.{id}.html")]
-        public IActionResult Catalog(int id, string keyword, int? pageSize, string sortBy, int page = 1)
+        public IActionResult Catalog(int id, int? pageSize, string sortBy, int page = 1)
+        {
+            var catalog = new CatalogViewModel();
+            ViewData["BodyClass"] = "shop_grid_full_width_page";
+            if (pageSize == null)
+                pageSize = _configuration.GetValue<int>("PageSize");
+
+            catalog.PageSize = pageSize;
+            catalog.SortType = sortBy;
+            catalog.Data = _productService.GetAllPaging(id, string.Empty, page, pageSize.Value);
+            catalog.Category = _productCategoryService.GetById(id);
+
+            return View(catalog);
+        }
+
+        [Route("{alias}-p.{id}.html", Name = "ProductDetail")]
+        public IActionResult Details(int id)
         {
             return View();
         }
+
     }
 }
